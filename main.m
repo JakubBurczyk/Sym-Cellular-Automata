bb4409f07cb97c7452383fe2711c13fa6c771dee
Jakub Burczyk <jburczyk99@gmail.com>
2021-03-29 02:44:29 +0200

    the sickening

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 clc
 clear
-automat = automata(50,50,50);
+automat = automata(20,20,50);
 
-for i=1:100
+for i=1:400
     pause(0.0001)
     automat = automat.update();
     fprintf("Update cycle %i \n",i)
